3308294cfa204f540ce824130ec6b6dd2a89043a
Carl Tablante <carl.q.tablante@gmail.com>
2015-05-16 19:02:33 +0800

    STI on user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150513103747) do
+ActiveRecord::Schema.define(version: 20150516085110) do
 
   create_table "forms", force: :cascade do |t|
     t.string   "letter_code"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 20150513103747) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "type"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
